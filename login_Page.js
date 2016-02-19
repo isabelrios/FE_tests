8ba1cfbb7aa90a182394f414c80cb626c8eb98b0
isabelrios <irios@MacBook-Pro-de-Isabel.local>
2016-02-19 19:45:07 +0100

    cleaning login_page

--- login_Page.js
+++ login_Page.js
@@ -7,13 +7,6 @@ function LoginPage(driver) {
     this.url = 'http://localhost:3000';
 };
 
-LoginPage.Selectors = {
-    pwdSelector: webdriver.By.id('pwd1-input'),
-    pwdConfirmSelector: webdriver.By.id('pwd2-input'),
-    submit: webdriver.By.id('set-button'),
-    successMessage: webdriver.By.id('thank-you')
-};
-
 LoginPage.prototype = {
     openPage : function() {
         this.driver.get(this.url);

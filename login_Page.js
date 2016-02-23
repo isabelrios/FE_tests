7be920101670f63abc0b1951b40da329f5575a4e
isabelrios <irios@MacBook-Pro-de-Isabel.local>
2016-02-23 16:26:21 +0100

    adding promises

--- login_Page.js
+++ login_Page.js
@@ -1,18 +1,22 @@
 'use strict';
 
+var config = require('./config.js');
 var webdriver = require('selenium-webdriver');
- 
+var SetUpView = require('./lib/view/main_page/view.js');
+var setUpView;
+
 function LoginPage(driver) {
+
     this.driver = driver;
-    this.url = 'http://localhost:3000';
+    this.url = config.BASE_URL
+    setUpView = new SetUpView(this.driver);
 };
 
 LoginPage.prototype = {
     openPage : function() {
-        this.driver.get(this.url);
-        var SetUpView = require('./lib/view/main_page/view.js');
-        var setUpView = new SetUpView(this.driver);
+      return this.driver.get(this.url).then(function(){
         return setUpView;
+      });
     }
 };
 
--- test_login_ac_view.js
+++ test_login_ac_view.js
@@ -9,6 +9,8 @@ var mochaTimeOut = 30000; //ms
 var driver;
 var loginPage;
 var mainPage;
+var successPage;
+var newApp;
 
 test.before(function() {
     this.timeout(mochaTimeOut);
@@ -21,45 +23,72 @@ test.before(function() {
 test.describe( 'Setup Page', function() {
 
     beforeEach(function() {
-         mainPage = loginPage.openPage();
+        return loginPage.openPage().then(function(setUpView) {
+            return mainPage = setUpView;
+        });
     });
 
-    test.it('shows pwd field', function() {
-        mainPage.pwdFieldPresent().then(function(present) {
-            assert.equal(present, true);
-        });;
-
+    test.it('shows password field', function() {
+        return mainPage.passwordField().then(function(present) {
+            return assert.equal(present, true);
+        });
     });
     
-    test.it('shows pwd confimation field', function() {
-        mainPage.pwdConfirPresent().then(function(text) {
-            assert.equal(text, true);
-        
+    test.it('shows password confimation field', function() {
+        return mainPage.passwordConfirmField().then(function(present) {
+            return assert.equal(present, true);
         });
     });
 
-    test.it('introduce correct pwd', function() {
-        mainPage.typePwd('12345678');
-        mainPage.reTypePwd('12345678');
-        mainPage.accpet();
-
-        mainPage.successLogin().then(function(text) {
-            assert.equal(text, 'Thank you!');
+    test.it('introduce correct password', function() {
+        return mainPage.typePassword('12345678').then(function() {
+            return mainPage.confirmTypePassword('12345678')
+        }).then(function(){
+            return mainPage.accpet()
+        }).then(function(successfulPageView){
+            return successPage = successfulPageView
+        }).then(function(){
+            return successPage.successLogin()
+        }).then(function(text){
+            return assert.equal(text, 'Thank you!')
         });
     });
 
-    test.it('introduce incorrect pwd', function() {
-        mainPage.typePwd('12345678');
-        mainPage.reTypePwd('123456');
-        mainPage.accpet();
-
-        mainPage.incorrectLogin().then(function(text){
+    test.it('introduce incorrect password', function() {
+        return mainPage.typePassword('12345678').then(function(){
+            return mainPage.confirmTypePassword('123456')
+        }).then(function(){
+            return mainPage.accpet()
+        }).then(function(){
+            return mainPage.incorrectLogin()
+        }).then(function(text){
             assert.equal(text, 'Passwords don\'t match! Please try again.')
+        }).then(function(){
+            return mainPage.alertAccept();
         });
-
-        mainPage.alertAccept();
     });
 
+    test.it('open link to go to the app', function() {
+        return mainPage.typePassword('12345678').then(function() {
+            return mainPage.confirmTypePassword('12345678')
+        }).then(function(){
+            return mainPage.accpet()
+        }).then(function(successfulPageView){
+            return successPage = successfulPageView
+        }).then(function(){
+            return successPage.successLogin()
+        }).then(function(text){
+            return assert.equal(text, 'Thank you!')
+        }).then(function(){
+            return successPage.openLinkToApp()
+        }).then(function(appPage){
+            return newApp = appPage
+        }).then(function(){
+            return newApp.getAppName()
+        }).then(function(text){
+            return assert.equal(text, 'Example Domain');
+        });
+    });
 });
 
 test.afterEach(function() {
